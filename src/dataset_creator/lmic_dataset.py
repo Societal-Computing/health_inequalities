f5f2844114eaa2145befea435ced83bfed73545a
taidoo <taidoo@pmintl.net>
2023-04-27 00:24:46 +0200

    Refactor code

--- src/dataset_creator/lmic_dataset.py
+++ src/dataset_creator/lmic_dataset.py
@@ -1,12 +1,13 @@
 # Import libraries.
-import os
 import json
+import os
 from pathlib import Path
-from typing import Dict, List
+from typing import Dict , List
 
 import geopandas as gpd
 import pandas as pd
 
+# TODO add logging
 
 class DataPreprocessing:
 
@@ -24,7 +25,7 @@ class DataPreprocessing:
         data = None
         data_path = Path(data_path)
         if data_path.suffix == '.xlsx':
-            data = pd.read_excel(data_path)
+            data = pd.read_excel(data_path, encoding='ISO-8859-1')
         elif data_path.suffix == '.csv':
             data = pd.read_csv(data_path, encoding='ISO-8859-1')
 
@@ -43,12 +44,15 @@ class DataPreprocessing:
         :return combined shapefiles:
         """
         all_shapefiles_ls = []
+        # logger.info("combing all shapefiles")
         for single_shapefilepath in shapefilepaths:
             all_shapefiles_ls.append(gpd.read_file(single_shapefilepath))
         combined_shapefile = pd.concat(all_shapefiles_ls)
+        # logger.info("all shapefiles combined")
         return combined_shapefile
     def main(self):
         for level in self.GADM_LEVELS:
+            # logger.info(f"combining dataset for Gadm {level}")
             level_config = self.config.pop(level)
             level_shapefiles = level_config["shapefiles_path"]
             africa_dataset_path = level_config["africa_dataset_path"]
@@ -58,10 +62,12 @@ class DataPreprocessing:
                 combined_dataset = pd.merge(combined_shapefile, africa_dataset,on="GID_0", how="inner")
                 saving_path = self.saving_path_for_gadm_file(level)
                 combined_dataset.to_csv(saving_path)
+                # logger.info(f"dataset for Gadm {level} complete and saved in {saving_path}")
             if level == "GADM_1":
                 combined_dataset = pd.merge(combined_shapefile, africa_dataset, on="GID_1", how="inner")
                 saving_path = self.saving_path_for_gadm_file(level)
                 combined_dataset.to_csv(saving_path)
+                # logger.info(f"dataset for Gadm {level} complete and saved in {saving_path}")
 
     def saving_path_for_gadm_file(self,level):
         if not os.path.exists(self.config['saving_path']):
@@ -70,7 +76,7 @@ class DataPreprocessing:
 
 
 if __name__ == '__main__':
-    config_path = "/home/theo/Documents/Societal_Computing/health_inequalities/config_scripts/dataset_config.json"
+    config_path = "config_scripts/dataset_config.json"
     with open(config_path) as pth:
         config = json.load(pth)
 
