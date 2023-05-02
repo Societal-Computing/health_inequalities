6ca1715c4944f9ec0e5cdc1dfaffdfc64bcd4c40
taidoo <taidoo@pmintl.net>
2023-05-02 17:00:02 +0200

    added sci file processing

--- src/dataset_creator/lmic_dataset.py
+++ src/dataset_creator/lmic_dataset.py
@@ -2,17 +2,21 @@
 import json
 import logging.handlers
 import os
+import warnings
 from pathlib import Path
-from typing import Dict, List
+from typing import Dict , List
 
 import geopandas as gpd
+import numpy as np
 import pandas as pd
+
+warnings.filterwarnings("ignore")
 
 # TODO move logging to file
 
 logger = logging.getLogger('health_inequalities')
 formatter = logging.Formatter(
-    fmt='%(filename)s | %(lineno)d | %(funcName)s | %(asctime)s | %(levelname)s: %(message)s',
+    fmt='%(filename)s | %(lineno)d | %(funcName)s | %(asctime)s | %(levelname)s: %(message)s' ,
     datefmt='%H:%M:%S'
 )
 logger.setLevel(logging.INFO)
@@ -22,11 +26,11 @@ consoleHandler.setLevel(logging.INFO)
 consoleHandler.setFormatter(formatter)
 logger.addHandler(consoleHandler)
 
+
 class DataPreprocessing:
+    GADM_LEVELS = ["GADM_0" , "GADM_1"]
 
-    GADM_LEVELS = ["GADM_0", "GADM_1", "GADM_2"]
-
-    def __init__(self, config_json: Dict) -> None:
+    def __init__(self , config_json: Dict) -> None:
         self.config = config_json
 
     @staticmethod
@@ -38,12 +42,12 @@ class DataPreprocessing:
         data = None
         data_path = Path(data_path)
         if data_path.suffix == '.xlsx':
-            data = pd.read_excel(data_path, encoding='ISO-8859-1')
+            data = pd.read_excel(data_path , encoding='ISO-8859-1')
         elif data_path.suffix == '.csv':
-            data = pd.read_csv(data_path, encoding='iso-8859-9')
+            data = pd.read_csv(data_path , encoding='ISO-8859-1')
         else:
             logger.error("Use a worksheet file of either csv or xlsx format")
-        return data.head()
+        return data
 
     @staticmethod
     def shapefile_reader(shapefile_path: str):
@@ -67,33 +71,54 @@ class DataPreprocessing:
         combined_shapefile = pd.concat(all_shapefiles_ls)
         logger.info(" all shapefiles combined for single level")
         return combined_shapefile
+
+    @staticmethod
+    def calculate_avg_std_SCI(sci_dataset: pd.DataFrame , lmic_iso2_names: List) -> pd.DataFrame:
+        """
+        Calculates avg amd standard deviation of sci_data
+        :param sci_dataset:
+        :param lmic_iso2_names:
+        :return: avg_std
+        """
+        meta_data = sci_dataset[sci_dataset.user_loc.isin(lmic_iso2_names)].copy()
+        avg_std = meta_data.groupby('user_loc').agg(Mean_SCI=('scaled_sci' , np.mean) ,
+                                                    Std_SCI=('scaled_sci' , np.std)).reset_index()
+        return avg_std
+
     def main(self):
         for level in self.GADM_LEVELS:
             logger.info(f"combining dataset for Gadm {level}")
             level_config = self.config.pop(level)
             level_shapefiles = level_config["shapefiles_path"]
             africa_dataset_path = level_config["africa_dataset_path"]
+            sci_dataset = pd.read_csv(level_config["sci_data_path"] , delimiter="\t")
             combined_shapefile = self.combine_shapefiles_single_gadm_level(list(level_shapefiles.values()))
             africa_dataset = self.worksheet_reader(africa_dataset_path)
             if level == "GADM_0":
-                combined_dataset = pd.merge(combined_shapefile, africa_dataset,on="GID_0", how="inner")
+                gadm_dhs_dataset = pd.merge(combined_shapefile , africa_dataset , on="GID_0" , how="inner")
+                lmic_iso2_names = gadm_dhs_dataset.iso2.unique().tolist()
+                aggregated_sci = self.calculate_avg_std_SCI(sci_dataset , lmic_iso2_names)
+                combined_dataset = pd.merge(gadm_dhs_dataset , aggregated_sci , left_on='iso2' ,
+                                            right_on='user_loc' , how='inner')
+                combined_dataset = combined_dataset.drop(columns=["user_loc"])
                 saving_path = self.saving_path_for_gadm_file(level)
-                combined_dataset.to_file(saving_path, driver="GPKG")
+                combined_dataset.to_file(saving_path , driver="GPKG")
                 logger.info(f"dataset for Gadm {level} complete and saved in {saving_path}")
             if level == "GADM_1":
-                # to drop duplicate columns
-                # https://stackoverflow.com/questions/14984119/python-pandas-remove-duplicate-columns
-                africa_dataset = africa_dataset.T.drop_duplicates().T
-                combined_dataset = pd.merge(combined_shapefile, africa_dataset, on="GID_1", how="inner")
-                saving_path = self.saving_path_for_gadm_file(level)
-                combined_dataset.to_file(saving_path, driver="GPKG")
-                logger.info(f"dataset for Gadm {level} complete and saved in {saving_path}")
-            if level == "GADM_2":
-                combined_dataset = pd.merge(combined_shapefile, africa_dataset, on="GID_2", how="inner")
-                saving_path = self.saving_path_for_gadm_file(level)
-                combined_dataset.to_file(saving_path, driver="GPKG")
+                africa_dataset = africa_dataset.drop(columns=['fbkey' , 'FB_key'])
+                gadm1_dhs_dataset = pd.merge(combined_shapefile , africa_dataset , on="GID_1" , how="inner")
+                gadm1_dhs_dataset['GID_1'] = gadm1_dhs_dataset.GID_1.apply(lambda x:
+                                                                           x.replace('.' , '').replace('_1' , ''))
+                lmic_gid1_names = gadm1_dhs_dataset.GID_1.unique().tolist()
+                aggregated_sci = self.calculate_avg_std_SCI(sci_dataset , lmic_gid1_names)
+                combined_dataset = pd.merge(gadm1_dhs_dataset , aggregated_sci , left_on='GID_1' ,
+                                            right_on='user_loc' , how='inner')
 
-    def saving_path_for_gadm_file(self, level):
+                saving_path = self.saving_path_for_gadm_file(level)
+                combined_dataset.to_file(saving_path , driver="GPKG")
+                logger.info(f"dataset for Gadm {level} complete and saved in {saving_path}")
+
+    def saving_path_for_gadm_file(self , level):
         if not os.path.exists(self.config['saving_path']):
             os.makedirs(self.config['saving_path'])
         return Path(self.config['saving_path']).joinpath(f"{level}.gpkg").as_posix()
